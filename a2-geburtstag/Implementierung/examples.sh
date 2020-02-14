1feb3dca5052a0d2f4930e57221c317cfe51cdb6
Richard Wohlbold <rigewo02@gmail.com>
2020-02-14 16:39:50 +0100

    2: Generate examples

---
+++ a2-geburtstag/Implementierung/examples.sh
@@ -0,0 +1,17 @@
+rm examples.tex
+TIME="%E"
+
+for i in {1..9}; do
+    echo "\\subsection{Ziffer $i}" >> examples.tex
+
+    for j in 2019 2030 2080 2980; do
+        echo "\\subsubsection{$j}" >> examples.tex
+        if [ "$j" = 2019 ]; then
+            echo "Der von meinem Programm gefundene Term hat genauso viele Ziffern wie das Beispiel aus der Aufgabenstellung:" >> examples.tex
+        fi
+        echo "\\begin{lstlisting}" >> examples.tex
+        echo "\$ command time 'time: %E' python main.py $j $i" >> examples.tex
+        { command time -f "%E" python main.py $j $i ; } >> examples.tex 2>&1
+        echo "\\end{lstlisting}" >> examples.tex
+    done
+done
